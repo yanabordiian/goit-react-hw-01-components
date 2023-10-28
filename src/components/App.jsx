acfe416ba57a99359df26b857bdbc40357cff60c
yanabordiian <98254244+yanabordiian@users.noreply.github.com>
2023-10-28 18:49:39 +0200

    task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import data from '../data/statistics-data.json';
 import { FriendList } from './FriendList/FriendList';
 import friends from '../data/friends.json';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
 export const App = () => {
   const {
     avatar: userAvatar,
@@ -27,7 +28,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
-      <TransactionHistory items={transactions}/>
+      <TransactionHistory transactions={transactions}/>
       
     </div>
   );
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,3 +1,23 @@
 import css from './TransactionHistory.module.css';
 
-export const TransactionHistory
+export const TransactionHistory = ({ transactions }) => (
+    <table className={css.table}>
+        <thead className={css.title}>
+    <tr>
+      <th>Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>
+  </thead>
+
+        <tbody>
+            {transactions.map(item => (
+                <tr key={item.id}>
+                    <td>{item.type}</td>
+                    <td>{item.amount}</td>
+                    <td>{item.currency}</td>
+                </tr>
+            ))}
+  </tbody>
+</table>
+)
